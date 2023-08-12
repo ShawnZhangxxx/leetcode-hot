a49b4e5a96406c17ecc2ee915440965c6c589e53
ShawnZhangxxx <393705733xxx@gmail.com>
2023-08-12 14:57:50 +0800

    1

--- hot-100/0019_remove-nth-node-from-end-of-list/solution.go
+++ hot-100/0019_remove-nth-node-from-end-of-list/solution.go
@@ -5,9 +5,49 @@ __date__ = '2023/03/09 11:10'
 
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func main() {
+	l := create(1, 2, 3, 4)
+	l = removeNthFromEnd2(l, 1)
+	print(l)
+}
+func create(Data ...int) *ListNode {
+	node := &ListNode{}
+	head := node
+	for i := 0; i < len(Data); i++ {
+		newNode := &ListNode{
+			Val: Data[i],
+		}
+		head.Next = newNode
+		head = head.Next
+	}
+	return node.Next
+}
+func print(l *ListNode) {
+	for l.Next != nil {
+		fmt.Println(l.Val)
+		l = l.Next
+	}
+	fmt.Print(l.Val)
+}
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	var f, s *ListNode = head, head
+	for i := 0; i < n; i++ {
+		f = f.Next
+	}
+	for f.Next != nil {
+		f = f.Next
+		s = s.Next
+	}
+	s.Next = s.Next.Next
+	return head
+
 }
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {

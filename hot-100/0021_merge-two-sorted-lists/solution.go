69c47fa1e00e8e24d36a0f5584e11cfe191f4df5
ShawnZhangxxx <393705733xxx@gmail.com>
2023-08-13 09:36:24 +0800

    1

--- hot-100/0021_merge-two-sorted-lists/solution.go
+++ hot-100/0021_merge-two-sorted-lists/solution.go
@@ -5,9 +5,61 @@ __date__ = '2023/03/09 14:24'
 
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func create(Data ...int) *ListNode {
+	node := &ListNode{}
+	head := node
+	for i := 0; i < len(Data); i++ {
+		newNode := &ListNode{
+			Val: Data[i],
+		}
+		head.Next = newNode
+		head = head.Next
+	}
+	return node.Next
+}
+func print(l *ListNode) {
+	for l.Next != nil {
+		fmt.Println(l.Val)
+		l = l.Next
+	}
+	fmt.Print(l.Val)
+
+}
+
+func main() {
+	l1 := create(1, 2, 2, 2, 3, 4, 5)
+	l2 := create(0, 3, 4, 5)
+	l := mergeTwoLists2(l1, l2)
+	print(l)
+}
+
+func mergeTwoLists2(l1, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	node := head
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			head.Next = l1
+			l1 = l1.Next
+		} else {
+			head.Next = l2
+			l2 = l2.Next
+		}
+		head = head.Next
+	}
+	if l1 == nil {
+		head.Next = l2
+	} else {
+		head.Next = l1
+	}
+	return node.Next
+
 }
 
 func mergeTwoLists(l1, l2 *ListNode) *ListNode {

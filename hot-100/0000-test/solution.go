0b351ebb9ad44df4cd45b513bcea1af1cf51b1d1
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-16 11:34:04 +0800

    1

---
+++ hot-100/0000-test/solution.go
@@ -0,0 +1,16 @@
+/*
+__author__ = 'robin-luo'
+__date__ = '2023/03/07 14:02'
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 11, 3, 9}
+	fmt.Println(arr[len(arr) -1: ])
+}
+
+
+
--- hot-100/0032_longest-valid-parentheses/solution.go
+++ hot-100/0032_longest-valid-parentheses/solution.go
@@ -5,7 +5,46 @@ __date__ = '2023/03/10 15:41'
 
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+func main()  {
+	l := longestValidParentheses2(")()))(((())()((((")
+	fmt.Println(l)
+}
+
+
+func longestValidParentheses2(s string) int {
+	maxLen := 0
+	size := len(s)
+	dp := make([]int, size)
+	stack :=  []rune{}
+
+	dp[0] = 0
+	if s[0] == '('{
+		stack = append(stack,'(')
+	}
+
+	for i := 1; i < size; i++ {
+		if s[i] == '(' {
+			stack = append(stack,'(')
+			dp[i] = dp[i-1]
+		}else { //')'
+			if len(stack) > 0 { //说明之前有( ,    如果=0,无意义,直接忽略不压栈
+				stack = stack[:len(stack)-1]
+				dp[i] = dp[i-1] +1
+			}else {
+				dp[i] = dp[i-1]
+			}
+		}
+		if dp[i] > maxLen {
+			maxLen = dp[i]
+		}
+	}
+	return  maxLen
+}
 
 func longestValidParentheses(s string) int {
 	maxLen := 0

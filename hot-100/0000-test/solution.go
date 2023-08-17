8a381d6d318b667bc655987107ad6b24a200dc0f
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-17 19:51:26 +0800

    1

--- hot-100/0000-test/solution.go
+++ hot-100/0000-test/solution.go
@@ -10,6 +10,7 @@ import "fmt"
 func main() {
 	arr := []int{1, 11, 3, 9}
 	fmt.Println(arr[len(arr) -1: ])
+	fmt.Println(arr[:len(arr) -1])
 }
 
 
--- hot-100/0039_combination-sum/solution.go
+++ hot-100/0039_combination-sum/solution.go
@@ -5,15 +5,73 @@ __date__ = '2023/03/13 19:15'
 
 package main
 
+import "fmt"
+
+//重点题 TODO
+func main()  {
+
+	ans := combinationSum2([]int{1,2,4,5,10},10)
+	fmt.Println(ans)
+}
+
+func combinationSum2(candidates []int,target int)(ans [][]int)  {
+	combine := []int {}
+	var dfs func(target,idx int)   //如果是def := func(){} 的形式不能递归调用
+	dfs = func (target int,index int) {
+			if index == len(candidates) {
+				return
+			}
+			if target == 0 {
+
+				ans  = append(ans,append([]int(nil),combine...)) //这么写的目的 是防止被引用,你append的只是地址,
+				//ans  = append(ans,combine)
+				return
+			}
+			dfs(target,index+1)
+			if target-candidates[index] >= 0  {//选择当前元素
+				combine = append(combine,candidates[index])
+				dfs(target-candidates[index],index)
+				combine = combine[:len(combine)-1] //这一步是回溯的核心
+			}
+		}
+		dfs(target,0)
+	return ans
+}
+
+
+//var candidates []int = []int{1,5,10}
+//var ans  [][]int = [][]int{}
+//
+//func dfs (target int,combine []int,index int) {
+//	if index == len(candidates) {
+//		return
+//	}
+//	if target == 0 {
+//		ans  = append(ans,combine)
+//		return
+//	}
+//	dfs(target,combine,index+1)
+//	if target > 0  {//选择当前元素
+//		combine = append(combine,candidates[index])
+//		dfs(target-candidates[index],combine,index)
+//	}
+//}
+
+
+
+
+
 func combinationSum(candidates []int, target int) [][]int {
 	var (
 		track []int
 		res   [][]int
 	)
-
 	backTracking(target, 0, 0, candidates, track, &res)
 	return res
 }
+
+
+
 
 func backTracking(target, sum, startIndex int, candidates, track []int, res *[][]int) {
 	if sum == target {

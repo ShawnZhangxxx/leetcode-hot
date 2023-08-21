32b93f489e3870e19c79c252079fbdf1e578ce60
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-21 16:52:28 +0800

    1

--- hot-100/0062_unique-paths/solution.go
+++ hot-100/0062_unique-paths/solution.go
@@ -5,6 +5,37 @@ __date__ = '2023/03/17 10:49'
 
 package main
 
+import "fmt"
+
+func main()  {
+	//[[1,3],[2,6],[8,10],[15,18]]
+	res := uniquePaths2(2,3)
+	fmt.Println(res)
+}
+
+func uniquePaths2(m, n int) int {
+	// dp: dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	//dp := [m][n]int{}
+	dp := make([][]int,m)//二维数组初始化 只能这样
+	for i, _ := range dp {
+		dp[i] = make([]int,n)
+	}
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+
+	for j := 0; j < n; j++ {
+		dp[0][j] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	fmt.Println(dp)
+	return dp[m-1][n-1]
+}
+
 func uniquePaths(m, n int) int {
 	// dp: dp[i][j] = dp[i-1][j] + dp[i][j-1]
 	dp := make([][]int, m)
--- hot-100/0064_minimum-path-sum/solution.go
+++ hot-100/0064_minimum-path-sum/solution.go
@@ -5,7 +5,42 @@ __date__ = '2023/03/18 19:45'
 
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	res := minPathSum2([][]int{{1,2,3},{4,5,6}})
+	fmt.Println(res)
+}
+
+
+
+
+func minPathSum2(grid [][]int) int {
+	m,n := len(grid),len(grid[0])
+	dp := make([][]int,m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int,n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+
+	for j := 1; j < m; j++ {
+		dp[j][0] = dp[j-1][0] + grid[j][0]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = int(math.Min(float64(dp[i-1][j]),float64(dp[i][j-1]))) + grid[i][j]
+		}
+	}
+	fmt.Println(dp)
+	return dp[m-1][n-1]
+}
+
 
 func minPathSum(grid [][]int) int {
 	if grid == nil {

632d6daa71cfaf3f75bc723306ffac754ead901d
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-21 11:00:52 +0800

    1

--- hot-100/0053_maximum-subarray/solution.go
+++ hot-100/0053_maximum-subarray/solution.go
@@ -5,7 +5,33 @@ __date__ = '2023/03/14 17:35'
 
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+func main()  {
+	res := maxSubArray2([]int{1,3,-1,-8,6,7,1})
+	fmt.Println(res)
+}
+
+func maxSubArray2(nums []int) int {
+	res := nums[0]
+	var sum int
+	for _, v := range nums {
+		if sum > 0 {
+			sum = sum + v
+		}else {
+			sum = v
+		}
+		res = int(math.Max(float64(sum),float64(res)))
+	}
+
+	return res
+}
+
+
+
 
 func maxSubArray(nums []int) int {
 	res := nums[0]

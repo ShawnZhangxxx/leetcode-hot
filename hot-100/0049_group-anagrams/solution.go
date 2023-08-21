00d5e14ff3fe0994c0d73de98c2f5ddab67628ed
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-21 10:53:32 +0800

    1

--- hot-100/0049_group-anagrams/solution.go
+++ hot-100/0049_group-anagrams/solution.go
@@ -6,9 +6,42 @@ __date__ = '2023/03/14 17:10'
 package main
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 )
+
+func main()  {
+	res := groupAnagrams2([]string{"abc","bac","def","fde","cab","a"})
+	fmt.Println(res)
+}
+
+func groupAnagrams2(strs []string)[][]string  {
+
+	dict := make(map[string][]string)
+	for _, str := range strs {
+		//将str转化成数组
+		strArr := strings.Split(str,"")
+		sort.Strings(strArr)
+		orderStr := strings.Join(strArr[:],"") //相当于新建切片
+		if _, ok := dict[orderStr];ok {
+			dict[orderStr] = append(dict[orderStr],str)
+		}else {
+			dict[orderStr] = []string{str}
+		}
+	}
+	var res  [][]string
+	for _, v := range dict {
+			res = append(res,v)
+	}
+
+	return res
+}
+
+
+
+
+
 
 func groupAnagrams(strs []string) [][]string {
 	dict := make(map[string][]string)

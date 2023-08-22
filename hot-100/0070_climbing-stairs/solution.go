952c108636a82a401e48170caf8bebbd87d0dfc8
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-22 09:46:36 +0800

    1

--- hot-100/0070_climbing-stairs/solution.go
+++ hot-100/0070_climbing-stairs/solution.go
@@ -5,6 +5,26 @@ __date__ = '2023/03/27 10:17'
 
 package main
 
+import "fmt"
+
+func main()  {
+     res :=	climbStairs2(10)
+     fmt.Println(res)
+}
+
+func climbStairs2(n int) int {
+	dp := make([]int,n+1)
+	dp[0] = 0
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+
+
 func climbStairs(n int) int {
 	dp := make([]int, n+1)
 	for i := range dp {

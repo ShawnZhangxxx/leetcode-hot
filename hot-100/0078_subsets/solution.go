b74975530d2ef1f3791f5b81e3445e51ec47096b
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-08-24 15:32:44 +0800

    1

--- hot-100/0078_subsets/solution.go
+++ hot-100/0078_subsets/solution.go
@@ -5,12 +5,33 @@ __date__ = '2023/06/26 15:18'
 
 package main
 
+import "fmt"
+
+func main()  {
+	res:= subsets2([]int{1,2,3})
+	fmt.Println(res)
+}
+
+func subsets2(nums []int)(ans [][]int) {
+	n := len(nums)
+	for mask:=0;mask < 1<<n;mask++	{
+		set := []int{}
+		for i,v := range nums {
+			if mask >> i & 1>0 {
+				set = append(set,v)
+			}
+		}
+		ans = append(ans,append([]int(nil),set...))
+	}
+	return
+}
+
+
 func subsets(nums []int) [][]int {
 	var (
 		track []int
 		res   [][]int
 	)
-
 	backTracking(nums, track, 0, &res)
 	return res
 }
--- hot-100/0079_word-search/solution.go
+++ hot-100/0079_word-search/solution.go
@@ -5,6 +5,52 @@ __date__ = '2023/06/26 16:58'
 
 package main
 
+import "fmt"
+
+func main()  {
+		res := exist2([][]byte{
+			{'a','b','c','e'},
+			{'s','f','c','s'},
+			{'d','a','c','b'},
+		},"abcccad")
+		fmt.Println(res)
+}
+
+func exist2(board [][]byte,word string)bool  {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+		  if  dfs2(board,word,0,0,0) {
+		  	return true
+		  }
+		}
+	}
+	return false
+
+}
+
+func dfs2(board [][]byte , word string,i int,j int,k int) bool  {
+	if i >= len(board) || i<0 || j >= len(board[0]) || j<0 || board[i][j] != word[k] {
+		return false
+	}
+	//能执行到这儿说明找到对的相应的字符
+	if k == len(word) - 1 {
+		return true
+	}
+
+	board[i][j] = '#' //防止回到起点
+	res := dfs2(board,word,i+1,j,k+1)  ||
+		dfs2(board,word,i-1,j,k+1) ||
+		dfs2(board,word,i,j+1,k+1) ||
+		dfs2(board,word,i,j-1,k+1)
+
+	return res
+
+}
+
+
+
+
+
 func exist(board [][]byte, word string) bool {
 	for i := range board {
 		for j := range board[0] {

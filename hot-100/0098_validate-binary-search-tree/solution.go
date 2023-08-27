b08504def8d66d723aba989016cadf25c0af5cf6
ShawnZhangxxx <393705733xxx@gmail.com>
2023-08-27 23:12:56 +0800

    1

--- hot-100/0098_validate-binary-search-tree/solution.go
+++ hot-100/0098_validate-binary-search-tree/solution.go
@@ -8,12 +8,63 @@
 
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func main() {
+	//       5
+	//   3	    	7
+	// 2   4
+	var root *TreeNode
+	root = insert(root, 5)
+	root = insert(root, 3)
+	root = insert(root, 7)
+	root = insert(root, 4)
+	root = insert(root, 2)
+	root.Left.Left.Left = &TreeNode{
+		Val: 8,
+	}
+	res := isValidBST2(root)
+	fmt.Println(res)
+
+}
+
+//初始化一个二叉树
+func insert(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	if val < root.Val {
+		root.Left = insert(root.Left, val)
+	} else {
+		root.Right = insert(root.Right, val)
+	}
+	return root
+
+}
+
+func isValidBST2(root *TreeNode) bool {
+	return isValid(root)
+}
+
+func isValid(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if root.Left != nil && root.Left.Val > root.Val {
+		return false
+	} else if root.Right != nil && root.Right.Val < root.Val {
+		return false
+	}
+	return isValid(root.Left) && isValid(root.Right)
 }
 
 func isValidBST(root *TreeNode) bool {

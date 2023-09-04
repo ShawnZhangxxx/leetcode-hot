2cae51e413869e32a6a6cdd7d5617bb00275cef5
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-09-04 11:24:59 +0800

    1

--- hot-100/0160_intersection-of-two-linked-lists/solution.go
+++ hot-100/0160_intersection-of-two-linked-lists/solution.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -17,7 +19,44 @@ type ListNode struct {
 
 //1,1,1
 //1,1 他俩的相交节点为
+func create(Data ...int) *ListNode {
+	node := &ListNode{}
+	head := node
+	for i := 0; i < len(Data); i++ {
+		newNode := &ListNode{
+			Val: Data[i],
+		}
+		head.Next = newNode
+		head = head.Next
+	}
+	return node.Next
+}
+func print(l *ListNode)  {
+	for l.Next != nil {
+		fmt.Println(l.Val)
+		l = l.Next
+	}
+	fmt.Println(l.Val)
 
+
+}
+func main()  {
+	////Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2
+	////, skipB = 3
+	l1 := create(4)
+	var i1 = &ListNode{Val: 1,Next: nil}
+	var i5 = &ListNode{Val: 7,Next: nil}
+	l1.Next = i1
+	l1.Next.Next = i5
+	print(l1)
+	l2 := create(5,6)
+	l2.Next.Next = i1
+	l2.Next.Next.Next = i5
+	print(l2)
+
+	res := getIntersectionNode(l1,l2)
+	fmt.Println(res.Val)
+}
 
 func getIntersectionNode(h1, h2 *ListNode) *ListNode {
 	l1, l2 := h1, h2

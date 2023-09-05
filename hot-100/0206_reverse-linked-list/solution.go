1cb86b4d227b7cfa4c330ddcd5fa9d930e4aa675
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-09-05 10:57:12 +0800

    1

--- hot-100/0206_reverse-linked-list/solution.go
+++ hot-100/0206_reverse-linked-list/solution.go
@@ -8,10 +8,49 @@
 
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func create(Data ...int )*ListNode  {
+	head := &ListNode{}
+	dumy := head
+	for i := 0; i < len(Data); i++ {
+		node := &ListNode{Val: Data[i]}
+		head.Next =  node
+		head = head.Next
+	}
+	return dumy.Next
+}
+
+func print(l *ListNode)  {
+	for l!= nil {
+		fmt.Println(l.Val)
+		l = l.Next
+	}
+}
+
+func main()  {
+	l := create(1,2,3,4,5,6,7,8)
+	print(l)
+	l = reverseList2(l)
+	print(l)
+}
+func reverseList2(head *ListNode) *ListNode {
+		var prev *ListNode
+		cur := head
+	for cur != nil {
+		tmp := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = tmp
+	}
+	return  prev
+}
+
 
 func reverseList(head *ListNode) *ListNode {
 	var prev *ListNode

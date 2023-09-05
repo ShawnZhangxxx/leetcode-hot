c639eceb0752208336d56b342a59dc43f726192a
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-09-05 10:28:55 +0800

    1

--- hot-100/0200_number-of-islands/solution.go
+++ hot-100/0200_number-of-islands/solution.go
@@ -8,6 +8,47 @@
 
 package main
 
+import "fmt"
+
+func main()  {
+	res := numIslands2([][]byte{
+		{ '1','1','0','0','0' },
+		{ '1','1','0','0','0' },
+		{ '0','0','1','0','0' },
+		{ '0','0','0','1','1' },
+	})
+	fmt.Println(res)
+}
+
+func numIslands2(grid [][]byte)int  {
+	var (
+		res int
+		dfs func(i,j int)
+	)
+	dfs = func(i, j int)  {
+		if i>= 0 && i<len(grid) && j>= 0 && j<len(grid[i]) && grid[i][j] == '1' {
+			grid[i][j] = '0'
+			dfs(i-1,j)
+			dfs(i+1,j)
+			dfs(i,j-1)
+			dfs(i,j+1)
+		}
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				res ++
+				dfs(i,j)
+			}
+		}
+	}
+	return  res
+	
+	
+}
+
+
+
 func numIslands(grid [][]byte) int {
 	var (
 		res int

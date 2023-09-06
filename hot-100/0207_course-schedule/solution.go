5f0dfe9c860295a11ddfaa365ddbb262a4dc8d48
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-09-06 10:29:27 +0800

    1

--- hot-100/0207_course-schedule/solution.go
+++ hot-100/0207_course-schedule/solution.go
@@ -8,6 +8,56 @@
 
 package main
 
+import "fmt"
+
+func main() {
+	res := canFinish2(4, [][]int{
+		{3, 1},
+		{2, 1},
+		{3, 2},
+		{1, 3},
+	})
+	fmt.Println(res)
+}
+func canFinish2(numCourses int, prerequisites [][]int) bool {
+	graph := make([][]int, numCourses)
+	queue := make([]int, 0)
+	inDegree := make([]int, numCourses)
+
+	for i := 0; i < len(prerequisites); i++ {
+		src := prerequisites[i][1]
+		dst := prerequisites[i][0]
+		if graph[src] == nil {
+			graph[src] = []int{}
+		}
+		graph[src] = append(graph[src], dst)
+		inDegree[dst] ++
+	}
+
+	//第一次初始化队列
+	for i := 0; i < numCourses; i++ {
+		if inDegree[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+	for len(queue) > 0 {
+		peek := queue[0]
+		queue = queue[1:]
+		for j := 0; j < len(graph[peek]); j++ {
+			inDegree[graph[peek][j]] --
+			if inDegree[graph[peek][j]] == 0 {
+				queue = append(queue, graph[peek][j])
+			}
+		}
+	}
+	for i := 0; i < len(inDegree); i++ {
+		if inDegree[i] > 0 {
+			return  false
+		}
+	}
+	return true
+}
+
 func canFinish(numCourses int, prerequisites [][]int) bool {
 	list := make([][]int, numCourses)
 	inDegree := make([]int, numCourses)

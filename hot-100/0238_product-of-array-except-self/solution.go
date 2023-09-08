1f7836962c6e25020e175ecd5cdb17b61a311d27
zhangxiang01 <zhangxiang01@mail.jj.cn>
2023-09-08 16:17:55 +0800

    1

--- hot-100/0238_product-of-array-except-self/solution.go
+++ hot-100/0238_product-of-array-except-self/solution.go
@@ -12,10 +12,11 @@ func productExceptSelf(nums []int) []int {
 	n := len(nums)
 	res := make([]int, n)
 	res[0] = 1
+	//去掉当前位置的前缀积
 	for i := 1; i < n; i++ {
 		res[i] = res[i-1] * nums[i-1]
 	}
-
+	//从右向左推,上面的*右侧的前缀积也是抛去当前元素
 	rightProduct := 1
 	for i := n - 1; i >= 0; i-- {
 		res[i] *= rightProduct
